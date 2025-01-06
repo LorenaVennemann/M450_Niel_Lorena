bddb58d18060f5451e423fdab9ede164a8e398be
LorenaVennemann <jil.vennemann@gmail.com>
2025-01-06 08:08:44 +0100

    test

--- ci-cd/recipe-planner-fronend-and-backend/recipe-planner-backend/target/generated-sources/annotations/ch/tbz/recipe/planner/mapper/IngredientEntityMapperImpl.java
+++ ci-cd/recipe-planner-fronend-and-backend/recipe-planner-backend/target/generated-sources/annotations/ch/tbz/recipe/planner/mapper/IngredientEntityMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-12-16T10:35:08+0100",
+    date = "2024-12-16T13:03:16+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.1 (Oracle Corporation)"
 )
 @Component
